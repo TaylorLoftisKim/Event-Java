1c1efecee4aab86ecd7a1f226d2dce0507a07e9f
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-17 11:27:12 -0700

    refactored object and tests

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,24 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Event {
+  private int mCost;
   private int mNumberOfGuests;
   private String mFood;
-  private String mBeverages;
+  private String mDrink;
   private String mEntertainment;
+  private int mCoupon;
 
-  public Event(int numberOfGuests, String food, String beverages, String entertainment) {
+  public Event(int cost, int numberOfGuests, String food, String drink, String entertainment, int coupon) {
+    mCost = cost;
     mNumberOfGuests = numberOfGuests;
     mFood = food;
-    mBeverages = beverages;
+    mDrink = drink;
     mEntertainment = entertainment;
+    mCoupon = coupon;
   }
-
-    public String getFood() {
-      return mFood;
-    }
-
-  // public int getNumberOfGuests() {
-  //   return mNumberOfGuests;
-  // }
-
 }
