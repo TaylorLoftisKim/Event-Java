dc8a76b06072d8a392f7c6424ec80cf1936e095c
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-17 08:48:57 -0700

    Make readme and additions to object and test files

---
+++ src/main/java/Event.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+  private String mNumberOfGuests;
+  private String mFood;
+  private String mBeverages;
+  private String mEntertainment;
+
+  }
+
+}
