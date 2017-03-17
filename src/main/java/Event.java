5538cc2d2112226f73b330a127f6eea72303f425
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-17 10:20:40 -0700

    Create second test, declare variables in Event Object

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,16 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Event {
-  // private int mNumberOfGuests;
-  // private String mFood;
-  // private String mBeverages;
-  // private String mEntertainment;
-  //
-  // public Event(int numberOfGuests, String food, String beverages, String entertainment) {
-  //   mNumberOfGuests = numberOfGuests;
-  //   mFood = food;
-  //   mBeverages = beverages;
-  //   mEntertainment = entertainment;
+  private int mNumberOfGuests;
+  private String mFood;
+  private String mBeverages;
+  private String mEntertainment;
+
+  public Event(int numberOfGuests, String food, String beverages, String entertainment) {
+    mNumberOfGuests = numberOfGuests;
+    mFood = food;
+    mBeverages = beverages;
+    mEntertainment = entertainment;
+  }
+
+    public String getFood() {
+      return mFood;
+    }
+
+  // public int getNumberOfGuests() {
+  //   return mNumberOfGuests;
   // }
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,11 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.lang.*;
 
 public class EventTest {
 
   @Test
   public void newEvent_instantiatesCorrectly() {
-    Event testEvent = new Event();
+    Event testEvent = new Event("pizza");
     assertEquals(true, testEvent instanceof Event);
   }
+
+  @Test
+  public void newEvent_getsFood_true() {
+    Event testEvent = new Event("pizza");
+    assertEquals("pizza", testEvent.getFood());
+  }
+  // @Test
+  // public void newEvent_getNumberOfGuests_1() {
+  //   Event testEvent = new Event(1);
+  //   assertEquals(1, testEvent.getNumberOfGuests());
+  // }
 }
